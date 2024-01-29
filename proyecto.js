8de8d72c927cc786cf7f83c95aa50c3e092c2b62
nuhagit <nuhaquintero1998@gmail.com>
2024-01-29 11:43:17 +0000

    Movimiento solventado + colisiones board

--- proyecto.js
+++ proyecto.js
@@ -8,18 +8,39 @@ const enemy3 = document.getElementById("enemy3")
 var xPlayer = 390
 var playerDirection = 1
 
-window.addEventListener('keydown', function(e) {
-    console.log("hola")
-    //var playerRight = xPlayer;
-    if (e.key === "d") {
-       xPlayer += 10;
+function movement(playerDirection){ 
+       xPlayer += 10 * playerDirection;
         player.style.left = xPlayer + "px";
     }
+    
 
-    if (e.key === "a") {
-        xPlayer -= 10;
-         player.style.left = xPlayer + "px";
+
+window.addEventListener('keydown', function(e) {  
+  
+    if (e.key === "d" && xPlayer < 780) {
+        playerDirection = 1
+        movement(playerDirection)
+    }
+
+    if (e.key === "a" && xPlayer > 0) {
+        playerDirection = -1
+        movement(playerDirection)
      }
 }) 
 
+// //function checkCollision(){
+  
+//     //if (xPlayer <= 10 || xPlayer >= 780) {
+//         //playerDirection = -1 
+//         return true
+//     } else { return false}
+// }
+        
 
+    
+
+
+
+    
+    
+     
